9d1e1c04193ce25d62afc96c46aac369b1754461
Harald Kriebisch <haraldkriebisch@MBP-von-Harald.fritz.box>
2024-01-17 22:48:50 +0100

    Add form field validations
    https://docs.flutter.dev/cookbook/forms/validation

--- lib/main.dart
+++ lib/main.dart
@@ -50,6 +50,8 @@ class _SignUpFormState extends State<SignUpForm> {
 
   double _formProgress = 0;
 
+  final _formKey = GlobalKey<FormState>();
+
   void _updateFormProgress() {
     double progress = 0.0;
     final controllers = [
@@ -71,9 +73,16 @@ class _SignUpFormState extends State<SignUpForm> {
     Navigator.of(context).pushNamed('/welcome');
   }
 
+  void _onFormSubmit() {
+    if (_formKey.currentState!.validate()) {
+      _showWelcomeScreen();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Form(
+      key: _formKey,
       onChanged: _updateFormProgress,
       child: Column(
         mainAxisSize: MainAxisSize.min,
@@ -85,20 +94,46 @@ class _SignUpFormState extends State<SignUpForm> {
             child: TextFormField(
               controller: _firstNameTextController,
               decoration: const InputDecoration(hintText: 'First name'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                if (value.contains(RegExp(r'[0-9]'))) {
+                  return 'Your name must not contain any numbers';
+                }
+                return null;
+              },
             ),
           ),
           Padding(
             padding: const EdgeInsets.all(8),
             child: TextFormField(
-              controller: _lastNameTextController,
-              decoration: const InputDecoration(hintText: 'Last name'),
-            ),
+                controller: _lastNameTextController,
+                decoration: const InputDecoration(hintText: 'Last name'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  if (value.contains(RegExp(r'[0-9]'))) {
+                    return 'Your name must not contain any numbers';
+                  }
+                  return null;
+                }),
           ),
           Padding(
             padding: const EdgeInsets.all(8),
             child: TextFormField(
               controller: _usernameTextController,
               decoration: const InputDecoration(hintText: 'Username'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                if (value.length < 3) {
+                  return 'Your name must contain at least three characters';
+                }
+                return null;
+              },
             ),
           ),
           TextButton(
@@ -114,7 +149,7 @@ class _SignUpFormState extends State<SignUpForm> {
                     : Colors.blue;
               }),
             ),
-            onPressed: _formProgress == 1.0 ? _showWelcomeScreen : null,
+            onPressed: _formProgress == 1.0 ? _onFormSubmit : null,
             child: const Text('Sign up'),
           ),
         ],
